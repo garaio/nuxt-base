785509352c1e220a97dbef2387067ab6c1cc0fee
Fabian Gander <fabian.gander@garaio.com>
2022-12-20 23:09:55 +0100

    Installing SASS

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
+  <div class="app">
     <NuxtWelcome />
   </div>
 </template>
+
+
+<style lang="scss" scoped>
+@import "./assets/styles/main.scss";
+
+.app {
+  background-color: #{$backgroundColor};
+}
+</style>
