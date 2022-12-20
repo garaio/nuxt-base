94ba5c63f624b1cc87bb8c491e751fbb60c9ef6f
Fabian Gander <fabian.gander@garaio.com>
2022-12-20 20:39:39 +0100

    Exercise Options API
    * defineNuxtComponent
    * jsonplaceholder
    * public folder assets

--- app.vue
+++ app.vue
@@ -1,5 +1,45 @@
+<script>
+import { defineNuxtComponent } from '#app';
+
+export default defineNuxtComponent({
+  created() {
+    this.fetchTodoList();
+  },
+  data: () => ({
+    todoList : []
+  }),
+  computed: {
+    numberOfTodos() {
+      return this.todoList.length;
+    },
+    completedTodos() {
+      return this.todoList.filter(todo => todo.completed).length;
+    }
+  },
+  methods: {
+    fetchTodoList() {
+      fetch("https://jsonplaceholder.typicode.com/todos/")
+        .then(response => response.json())
+        .then(json => {
+          this.todoList = json;
+        });
+    }
+  }
+})
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Hello Nuxt App</h1>
+    <img src="/background.jpg" width="300"/> 
+    <h2>Number of todos: {{ completedTodos }} / {{ numberOfTodos }}</h2>
+    <button @click="fetchTodoList()">Fetch todo list</button>
+    <!-- <pre>{{ todoList }}</pre> -->
+    <ul>
+      <li v-for="todo in todoList" :key="todo.id">
+        <input type="checkbox" v-model="todo.completed" />
+        {{ todo.title }} - {{ todo.completed }}
+      </li>
+    </ul>
   </div>
 </template>
