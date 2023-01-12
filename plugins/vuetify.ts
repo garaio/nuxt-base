8bb3d267bba02644ab69cfa176a791b3ca14db55
Fabian Gander <fabian.gander@garaio.com>
2023-01-12 09:19:31 +0100

    Adding vuetify theme handling

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,4 +1,5 @@
 // plugins/vuetify.js
+import type { ThemeDefinition } from "vuetify";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
@@ -6,10 +7,34 @@ import * as directives from "vuetify/directives";
 import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
 
+const myCustomLightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    "background": "#FFFFFF",
+    "surface": "#FFFFFF",
+    "primary": "#6200EE",
+    "primary-darken-1": "#3700B3",
+    "secondary": "#03DAC6",
+    "secondary-darken-1": "#018786",
+    "error": "#B00020",
+    "info": "#2196F3",
+    "success": "#4CAF50",
+    "warning": "#FB8C00",
+    "vue-green": "#42b983",
+    "nuxt-green": "#00DC82",
+  },
+};
+
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     components,
     directives,
+    theme: {
+      defaultTheme: "myCustomLightTheme",
+      themes: {
+        myCustomLightTheme,
+      },
+    },
   });
 
   nuxtApp.vueApp.use(vuetify);
